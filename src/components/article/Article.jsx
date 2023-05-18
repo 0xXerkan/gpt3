0923c3e23935715469da4de2a7ee56d27ac2b495
Ricky Landry <0xXerkan@gmail.com>
2023-05-18 02:49:54 -0400

    added Blog Section

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './article.css'
 
-const Article = () => {
+const Article = ({ imgUrl, date, title }) => {
   return (
-    <div>Article</div>
+    <div className='gpt3__blog__article-container'>
+      <div className='gpt3__blog__article-image'>
+        <img src={imgUrl} alt="blog" />
+      </div>
+      <div className='gpt3__blog__article-content'>
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>Read Full Article</p>
+      </div>
+    </div>
   )
 }
 
